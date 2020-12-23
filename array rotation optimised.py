8af224f63fe40741f7128daeba9bfccc08f99ee5
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2020-12-23 09:13:21 +0530

    Add files via upload

---
+++ array rotation optimised.py
@@ -0,0 +1,35 @@
+def reverserightalgo(arr,i,j):
+    while(i<j):
+        temp=arr[i]
+        arr[i]=arr[j]
+        arr[j]=temp
+        i+=1
+        j-=1
+
+def reverseleftalgo(arr,i,j):
+    while(i<j):
+        temp=arr[i]
+        arr[i]=arr[j]
+        arr[j]=temp
+        i+=1
+        j-=1
+def printarr(arr,n):
+    for i in range(n):
+        print(arr[i],end=' ')
+    print()
+def main():
+    t=int(input())
+    while(t>0):
+        n,k=map(int,input().split())
+        arr=list(map(int,input().split()))
+        reverserightalgo(arr,n-k,n-1)
+        reverserightalgo(arr,0,n-k-1)
+        reverserightalgo(arr,0,n-1)
+        printarr(arr,n)
+        reverseleftalgo(arr,0,k-1)
+        reverseleftalgo(arr,k,n-1)
+        reverseleftalgo(arr,0,n-1)
+        printarr(arr,n)
+        t=t-1
+
+main()
